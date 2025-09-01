678070ba71c1b06cda210c4e65f23ac40cca752e
Yogeshwaran-18 <yogeshwaran1888@gmail.com>
2025-09-01 16:18:30 +0530

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,190 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+import food from './assets/tech_logo/food.png';
+import dental from './assets/tech_logo/dental.png';
+import weather from './assets/tech_logo/weather.png';
+import plant from './assets/tech_logo/plant.png';
+import art from './assets/tech_logo/art.png';
+import images from './assets/tech_logo/images.png';
+import dci from './assets/tech_logo/dci.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+    
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+  
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: dci,
+      role: "Frontend Developer Intern",
+      company: "Dot com infoway",
+      date: "Jan 2025 - June 2025",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+      ],
+    },
+   ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: images,
+      school: "KLN COLLEGE OF ENGINEERING, Madurai",
+      date: "Sept 2023 - July 2025",
+      grade: "75.1%",
+      desc: "I have completed my MCA at KLN College of Engineering, Madurai (affiliated to Anna University), where I built strong foundations in software development, data structures, databases, and web technologies. Along with academic learning, I worked on projects in MERN stack development and deep learning, preparing me to begin my career as a Full Stack Developer. software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
+      degree: "Master of Computer Applications - MCA",
+    },
+    {
+      id: 1,
+      img: art,
+      school: "KLN ARTS AND SCIENCE COLLGE, Madurai",
+      date: "Aug 2020 - May 2023",
+      grade: "71.1%",
+      desc: "I have completed my B.Sc. in Computer Science from KLN Arts and Science College (Alagappa University). These academic experiences, along with my internship and projects, have helped me build strong foundations in software development, databases, and full-stack web technologies.Science (B.Sc.) from KLN ARTS AND SCIENCE COLLEGE, Madurai. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+      degree: "Bachelor of Science - BSC (Computer Science)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Online Food Website",
+      description:
+        " Developed a fully responsive food ordering web app with React.js, improving user experience and engagement. Implemented user authentication and cart functionality using Redux/Context API.",
+      image: food,
+      tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+      github: "https://github.com/Yogeshwaran-18/Online-Food-Website",  
+    },
+    {
+      id: 1,
+      title: "Plant Disease Prediction",
+      description:
+        " Created a CNN model using TensorFlow and Keras, achieving 97.8% training and 94.6% validation accuracy. Improved model robustness and reduced prediction errors significantly.",
+      image: plant,
+      tags: ["Python","CNN","Deep Learning","ML","StreamLit"],
+      github: "https://github.com/Yogeshwaran-18/Plant-Disease-Prediction",
+      webapp: "https://csprep.netlify.app/",
+    },
+    {
+      id: 2,
+      title: "Weather App",
+      description:
+        "Built a mobile-responsive weather forecasting web app using React.js and OpenWeatherMap API. Integrated live weather data with hourly and weekly forecasts, animated UI with Framer Motion, and user location detection using geolocation. Implemented dark mode and temperature unit toggle, enhancing usability and performance across devices.",
+      image: weather,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Yogeshwaran-18/Weather-App",
+      webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Dental Appointment Booking System",
+      description:
+        "Developed a Streamlit-based web app for dental appointment scheduling with dynamic form inputs and Lottie animations for enhanced user engagement.Integrated with n8n via webhooks to automate confirmation emails using JSON payloads, eliminating the need for atraditional backend.Implemented 12-hour time formatting, custom styling, and responsive layout for a seamless cross-device experience.",
+      image: dental,
+      tags: ["AI","Automation","StreamLit","n8n workflow"],
+      github: "https://github.com/Yogeshwaran-18/Dental-Booking-System-using-n8n",
+      webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+    },
+    
+  ];  
